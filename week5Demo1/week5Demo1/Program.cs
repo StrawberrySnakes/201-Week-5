7fe2ccfba2528269ecd66b7f710af616cd11e9ca
Dessa Shapiro <110559444+StrawberrySnakes@users.noreply.github.com>
2024-09-24 09:19:58 -0400

    added c# demo code

---
+++ week5Demo1/week5Demo1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace week5Demo1
+{
+    internal class Program
+    {
+
+        public static string printStuff(int num, string name)
+        {
+            string numString = num.ToString();
+            return name + numString;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int myFavoriteNum = 3;
+            int bestNumber = 7;
+            int sum;
+
+            bool thisIsTrue = false;
+            string myName = "dessa";
+            float myFloat = (float)4.7;
+
+            
+
+            string numberString = bestNumber.ToString();
+
+
+            sum = bestNumber + myFavoriteNum;
+
+            Console.WriteLine(sum + " "+printStuff(5, "Kate")+ " "+myFloat);
+
+            
+
+        }
+     }
+}
+
